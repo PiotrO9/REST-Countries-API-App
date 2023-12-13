b7ec1b780c6e7b41433dc62b25e28c03a9f545dc
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-12-13 19:36:49 +0100

    first commit

---
+++ router.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { Router } from 'vue-router';
+
+createApp(App).mount('#app');
