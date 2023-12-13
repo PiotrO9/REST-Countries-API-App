0436041fb9fa9beda85a8f143f82949117b749b9
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-12-13 20:40:04 +0100

    Add styles for nav component

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,11 +1,81 @@
 <template>
-    <nav>
+    <nav class="nav">
+        <span class="heading-text">
+            Where in the world?
+        </span>
 
+        <div class="theme-switcher">
+            <div class="icon-container">
+                <img v-if="isDarkTheme" src="../../public/assets/light-moon.svg" class="icon" alt="Moon icon">
+                <img v-else src="../../public/assets/dark-moon.svg" class="icon" alt="Moon icon">
+            </div>
+            <div class="current-mode">
+                {{ themeName }}
+            </div>
+        </div>
     </nav>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useThemeStore } from '../stores/themeStore';
+
+const themeStore = useThemeStore();
+
+const themeName = computed(() => {
+    return (themeStore.$state.isDark ? 'Dark' : 'Light') + " mode";
+})
+
+const isDarkTheme = computed(() => {
+    return themeStore.$state.isDark;
+})
 
 </script>
 
-<style scoped lang="css"></style>
+<style scoped>
+.nav {
+    display: flex;
+    padding: 10px;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--dark-blue);
+
+
+}
+
+.heading-text {
+    font-weight: 700;
+
+    @media (min-width: 768px) {
+        font-size: 26px;
+    }
+}
+
+.theme-switcher {
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+
+    @media (min-width: 768px) {
+        font-size: 18px;
+    }
+}
+
+.icon-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.icon {
+    height: 30px;
+    width: 30px;
+}
+
+.current-mode {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+</style>
